c7d56136b6dabf65fb6499e91c8b49a25482c0dd
Gen Vikat <gen@veebisepad.ee>
2022-10-05 11:13:18 +0300

    Connect API instance with API credentials, created .env file for security

--- api/api.js
+++ api/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "",
-    headers: {}
+    baseURL: import.meta.env.VITE_BASE_URL,
+    headers: {
+        "Accept-Version": "v1",
+        "Authorization": `Client-ID ${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`
+    }
 })
 
 export default API;
