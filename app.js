491fa2176fd7875b0da96c6b7868f96c1a8341d3
Gen Vikat <gen@veebisepad.ee>
2022-10-06 15:50:57 +0300

    Init Intersection Observer and get images data

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 import API from './api/api';
 
-API.get('/photos').then((response) => console.log(response.data))
+const imagesGridContainer = document.querySelector('.grid-gallery');
+
+API.get('/photos').then((response) => {
+    const images = response.data;
+
+    images.forEach((image) => {
+        const imageElement = new Image();
+        imageElement.src = image.urls.small;
+        imagesGridContainer.append(imageElement);
+    })
+
+    const imageElements = document.querySelectorAll('img');
+    
+
+    let observer = new IntersectionObserver((elements, observer) => {
+        imageElements.forEach(image => {
+            console.log(elements)
+            observer.observe(image);
+        })
+    });
+
+});
