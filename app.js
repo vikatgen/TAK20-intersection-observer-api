cd127ac08e96920573e4232e43a0e2b27730d883
Gen Vikat <gen@veebisepad.ee>
2022-10-12 09:19:49 +0300

    Implemented searching functionality with debounce. New images will show up.

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 import API from './api/api';
+import _ from 'lodash';
 
 const imagesGridContainer = document.querySelector('.grid-gallery');
+const searchInput = document.querySelector('#search');
 
-API.get('/photos').then((response) => {
-    const images = response.data;
+const fillImagesContainer = (images) => {
+    imagesGridContainer.innerHTML = ''
 
     images.forEach((image) => {
         const imageElement = new Image();
         imageElement.src = image.urls.small;
         imagesGridContainer.append(imageElement);
     })
+}
 
-    const imageElements = document.querySelectorAll('img');
-    
 
-    let observer = new IntersectionObserver((elements, observer) => {
-        imageElements.forEach(image => {
-            console.log(elements)
-            observer.observe(image);
-        })
-    });
+await API.get('/photos').then((response) => fillImagesContainer(response.data));
 
-});
+
+const fetchSearchedImages = _.debounce( async (event) => {
+    const response = await API.get('/search/photos', { params: { 
+        'query': event.target.value,
+        'per_page': 20
+     }})
+
+     fillImagesContainer(response.data.results)
+}, 1000);
+
+searchInput.addEventListener('input', fetchSearchedImages)
